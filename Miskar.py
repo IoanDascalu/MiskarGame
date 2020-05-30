2f794ac2366d1134c5a78c3e18f35d7b6c2f4f5c
IoanDascalu <idascalu@ithaca.edu>
2020-05-30 12:16:49 -0400

    Added a Miskar Module and a bit of code to main

---
+++ Miskar.py
@@ -0,0 +1,22 @@
+import pygame
+class Miskar(object):
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.left = True
+        self.right = False
+        self.up = False
+        self.down = False
+        self.standing = True
+        self.hitbox = (self.x, self.y - 2, 37, 55)
+        self.walkCount = 0
+
+    def existance(self):
+        print("I exist")
+
+    def draw(self, win):
+        self.hitbox = (self.x, self.y - 2, 37, 55)
+        pygame.draw.rect(win,(255,0,0), self.hitbox, 2)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import time
+from Miskar import Miskar
 
 pygame.init()
 
@@ -11,7 +12,30 @@ clock = pygame.time.Clock()
 
 pygame.display.set_caption("Miskar The Germaphobe")
 
+
+def movement(keyPressed):
+    if keyPressed[pygame.K_LEFT]:
+        pass
+    elif keyPressed[pygame.K_RIGHT]:
+        pass
+
+    elif keyPressed[pygame.K_DOWN]:
+        pass
+
+    elif keyPressed[pygame.K_UP]:
+        print("up")
+    pass
+
+
 run = True
+Miskar=Miskar(100, 100, 50, 50)
+
+Miskar.existance()
+
+
+def redrawGameWindow():
+    Miskar.draw()
+
 while run:
     clock.tick(60)
 
@@ -19,5 +43,10 @@ while run:
 
         if event.type == pygame.QUIT:
             run = False
+
+    keys = pygame.key.get_pressed()
+    movement(keys)
+
+    
 
 pygame.quit()
