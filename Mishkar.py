fa7086164113aaa8c2fe81b2a8093b8db897228e
IoanDascalu <idascalu@ithaca.edu>
2020-05-30 23:51:06 -0400

    Cleaned up currDir and facing

--- Mishkar.py
+++ Mishkar.py
@@ -14,14 +14,10 @@ class Mishkar(object):
         self.health = 100
         self.vel=5
         # self.inventory = Item.Item("MishkarBests/unicorn.png", "unicorn")
-        self.left = True
-        self.right = False
-        self.up = False
-        self.down = False
-        self.standing = True
-        self.collision_box = Rect(self.x, self.y - 2, 37, 55)
+        self.facing = 'Down'
+        self.currDir = 'Stand'
         self.walkCount = 0
-        self.collisionbox = (self.x, self.y - 2, 37, 55)
+        self.collision_box = (self.x, self.y - 2, 37, 55)
         self.downFig = [pygame.image.load(img) for img in listOfFigures[0]]
         self.upFig = [pygame.image.load(img) for img in listOfFigures[1]]
         self.leftFig = [pygame.image.load(img) for img in listOfFigures[2]]
@@ -36,79 +32,70 @@ class Mishkar(object):
     def draw(self):
         if self.walkCount + 1 >= 9:
             self.walkCount = 0
-        if not self.standing:
-            if self.left:
+        if self.currDir is not 'Stand':
+            if self.currDir is 'Left':
                 self.win.blit(self.leftFig[self.walkCount // 2], (self.x, self.y))
                 self.walkCount += 1
-            elif self.right:
+            elif self.currDir is 'Right':
                 self.win.blit(self.rightFig[self.walkCount // 2], (self.x, self.y))
                 self.walkCount += 1
-            elif self.up:
+            elif self.currDir is 'Up':
                 self.win.blit(self.upFig[self.walkCount // 2], (self.x, self.y))
                 self.walkCount += 1
-            elif self.down:
+            elif self.currDir is 'Down':
                 self.win.blit(self.downFig[self.walkCount // 2], (self.x, self.y))
                 self.walkCount += 1
         # else:
-        elif self.standing:
-            if self.left:
+        elif self.currDir is 'Stand':
+            if self.facing is 'Left':
                 self.win.blit(self.leftFig[0], (self.x, self.y))
-            if self.right:
+            if self.facing is 'Right':
                 self.win.blit(self.rightFig[0], (self.x, self.y))
-            if self.up:
+            if self.facing is 'Up':
                 self.win.blit(self.upFig[0], (self.x, self.y))
-            if self.down:
+            if self.facing is 'Down':
                 self.win.blit(self.downFig[0], (self.x, self.y))
 
         else:
-            if self.left:
+            if self.currDir is 'Left':
                 self.win.blit(self.leftFig[self.walkCount // len(self.leftFig)], (self.x, self.y))
                 self.walkCount += 1
-            elif self.right:
+            elif self.currDir is 'Right':
                 self.win.blit(self.rightFig[self.walkCount // len(self.leftFig)], (self.x, self.y))
                 self.walkCount += 1
-            elif self.up:
+            elif self.currDir is 'Up':
                 self.win.blit(self.upFig[self.walkCount // len(self.leftFig)], (self.x, self.y))
                 self.walkCount += 1
-            elif self.down:
+            elif self.currDir is 'Down':
                 self.win.blit(self.downFig[self.walkCount // len(self.leftFig)], (self.x, self.y))
                 self.walkCount += 1
-            self.collision_box = (self.x, self.y - 2, 37, 55)
-            pygame.draw.rect(self.win, (255, 0, 0), self.collision_box, 2)
+        self.collision_box = (self.x, self.y - 2, 37, 55)
+        pygame.draw.rect(self.win, (255, 0, 0), self.collision_box, 2)
 
     def movement(self, keyPressed):
 
         if keyPressed[pygame.K_LEFT]:
             self.x -= self.vel
-            self.left = True
-            self.right = False
-            self.up = False
-            self.down = False
-            self.standing = False
+            self.currDir = 'Left'
+            self.facing = 'Left'
+
         elif keyPressed[pygame.K_RIGHT]:
             self.x += self.vel
-            self.left = False
-            self.right = True
-            self.up = False
-            self.down = False
-            self.standing = False
-        elif keyPressed[pygame.K_DOWN]:
-            self.y += self.vel
-            self.left = False
-            self.right = False
-            self.up = False
-            self.down = True
-            self.standing = False
+            self.currDir = 'Right'
+            self.facing = 'Right'
+
         elif keyPressed[pygame.K_UP]:
             self.y -= self.vel
-            self.left = False
-            self.right = False
-            self.up = True
-            self.down = False
-            self.standing = False
-        else:
-            self.standing = True
+            self.currDir = 'Up'
+            self.facing = 'Up'
 
+        elif keyPressed[pygame.K_DOWN]:
+            self.y += self.vel
+            self.currDir = 'Down'
+            self.facing = 'Down'
+
+        else:
+            self.currDir = 'Stand'
 
     def interact(self):
         pass
